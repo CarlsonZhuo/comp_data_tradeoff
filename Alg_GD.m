b620c0a9642e3c5edd6a6f9f1b98838a9be9c1bc
CarlsonZhuo <carlsonzhuo@gmail.com>
2018-10-08 18:56:55 -0500

    proj GD bug fix

--- Alg_GD.m
+++ Alg_GD.m
@@ -8,13 +8,14 @@ p      = param.p;
 Lmax   = (0.25 * max(sum(X.^2,2)) + lambda);
 hist   = zeros(max_it,1);
 
-alpha  = 1 / (n*Lmax); % The learning rate
+% alpha  = 1 / (n*Lmax); 
+alpha  = 0.0005;
 w      = zeros(p, 1);
 
 for k = 1:max_it
     mu = X*(X'*w - y);
     w = w - alpha*mu;
-    w = sign(w) .* max(abs(w) - lambda, 0);
+    w = proj_L1_Linf(w, 1);
     hist(k) = norm(w - param.GD_w_hat);
 end
 w_estimated = w;
